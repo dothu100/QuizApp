593a92776bfdb06faac3a86587e492d0586cb0b7
dothu100 <dothianhthu100@gmail.com>
2026-09-03 04:02:29 +0700

    init commit

---
+++ app/src/main/java/com/example/quizapp/Activity/QuestionActivity.kt
@@ -0,0 +1,155 @@
+package com.example.quizapp.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.quizapp.Adapter.QuestionAdapter
+import com.example.quizapp.Domain.QuestionModel
+import com.example.quizapp.R
+import com.example.quizapp.databinding.ActivityQuestionBinding
+
+class QuestionActivity : AppCompatActivity(), QuestionAdapter.score {
+    private lateinit var binding: ActivityQuestionBinding
+    private var position: Int = 0
+    private var receivedList: MutableList<QuestionModel> = mutableListOf()
+    private var allScore = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQuestionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val window: Window = this@QuestionActivity.window
+        window.statusBarColor = ContextCompat.getColor(this@QuestionActivity, R.color.grey)
+
+        receivedList = intent.getParcelableArrayListExtra<QuestionModel>("list")!!.toMutableList()
+
+        binding.apply {
+            backBtn.setOnClickListener { finish() }
+
+            progressBar.progress = 1
+
+            questionTxt.text = receivedList[position].question
+            val drawableResourceId: Int = binding.root.resources.getIdentifier(
+                receivedList[position].picPath,
+                "drawable", binding.root.context.packageName
+            )
+            Glide.with(this@QuestionActivity)
+                .load(drawableResourceId)
+                .centerCrop()
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(60)))
+                .into(pic)
+        }
+
+        loadAnswers()
+
+        binding.rightArrow.setOnClickListener {
+            if (binding.progressBar.progress == 10) {
+                val intent = Intent(this@QuestionActivity, ScoreActivity::class.java)
+                intent.putExtra("Score", allScore)
+                startActivity(intent)
+                finish()
+                return@setOnClickListener
+            }
+            position++
+            binding.progressBar.progress = binding.progressBar.progress + 1
+            binding.questionNumberTxt.text = "Question ${binding.progressBar.progress}/10"
+            binding.questionTxt.text = receivedList[position].question
+
+            val drawableResourceId: Int = binding.root.resources.getIdentifier(
+                receivedList[position].picPath,
+                "drawable", binding.root.context.packageName
+            )
+            Glide.with(this@QuestionActivity)
+                .load(drawableResourceId)
+                .centerCrop()
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(60)))
+                .into(binding.pic)
+
+            loadAnswers()
+        }
+
+        binding.leftArrow.setOnClickListener {
+            if (binding.progressBar.progress == 1) {
+                return@setOnClickListener
+            }
+            position--
+            binding.progressBar.progress = binding.progressBar.progress - 1
+            binding.questionNumberTxt.text = "Question ${binding.progressBar.progress}/10"
+            binding.questionTxt.text = receivedList[position].question
+
+            val drawableResourceId: Int = binding.root.resources.getIdentifier(
+                receivedList[position].picPath,
+                "drawable", binding.root.context.packageName
+            )
+            Glide.with(this@QuestionActivity)
+                .load(drawableResourceId)
+                .centerCrop()
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(60)))
+                .into(binding.pic)
+
+            loadAnswers()
+        }
+    }
+
+    private fun loadAnswers() {
+        val users: MutableList<String> = mutableListOf()
+        users.add(receivedList[position].answer_1.toString())
+        users.add(receivedList[position].answer_2.toString())
+        users.add(receivedList[position].answer_3.toString())
+        users.add(receivedList[position].answer_4.toString())
+
+        if (receivedList[position].clickedAnswer != null) users.add(receivedList[position].clickedAnswer.toString())
+
+        val questionAdapter by lazy {
+            QuestionAdapter(
+                receivedList[position].correctAnswer.toString(), users, this,
+                onAnswered = {
+                    // Trì hoãn việc chuyển câu hỏi để người dùng thấy kết quả
+                    Handler().postDelayed({
+                        nextQuestion() // Chuyển câu hỏi sau một khoảng thời gian
+                    }, 1000) // Delay 1 giây (1000ms)
+                }
+            )
+        }
+        questionAdapter.differ.submitList(users)
+        binding.questionList.apply {
+            layoutManager = LinearLayoutManager(this@QuestionActivity)
+            adapter = questionAdapter
+        }
+    }
+
+    override fun amount(number: Int, clickedAnswer: String) {
+        allScore += number
+        receivedList[position].clickedAnswer = clickedAnswer
+    }
+
+    // Phương thức chuyển sang câu hỏi tiếp theo
+    fun nextQuestion() {
+        if (binding.progressBar.progress < 10) {
+            position++
+            binding.progressBar.progress = binding.progressBar.progress + 1
+            binding.questionNumberTxt.text = "Question ${binding.progressBar.progress}/10"
+            binding.questionTxt.text = receivedList[position].question
+
+            val drawableResourceId: Int = binding.root.resources.getIdentifier(
+                receivedList[position].picPath,
+                "drawable", binding.root.context.packageName
+            )
+            Glide.with(this@QuestionActivity)
+                .load(drawableResourceId)
+                .centerCrop()
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(60)))
+                .into(binding.pic)
+
+            loadAnswers()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/Activity/ScoreActivity.kt
@@ -0,0 +1,28 @@
+package com.example.quizapp.Activity
+
+import android.content.Intent
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.databinding.ActivityScoreBinding
+
+class ScoreActivity : AppCompatActivity() {
+    var score: Int = 0
+    lateinit var binding: ActivityScoreBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        score = intent.getIntExtra("Score",0)
+
+        binding.apply {
+            scoreTxt.text = score.toString()
+            backBtn.setOnClickListener {
+                startActivity(Intent(this@ScoreActivity, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
